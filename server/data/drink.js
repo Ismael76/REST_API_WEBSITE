9dc45ba12cead2571d5f859784dd8494ebbb06e9
eheath30 <elliot.heath@btinternet.com>
2022-06-17 14:39:26 +0100

    drinks added to array

--- server/data/drink.js
+++ server/data/drink.js
@@ -19,6 +19,27 @@ const drinks = [
     name: "Coke",
     url: "https://delivery.pret.co.uk/dw/image/v2/ABBI_PRD/on/demandware.static/-/Sites-pret-uk-master-catalog/default/dw1342af06/products/Cold%20Drinks/UK001022_RegularBottle_01_607.jpg?sw=584&sh=584",
   },
+  {
+    name: "BubbleTea",
+    url: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Bubble_Tea.png",
+  },
+  {
+    name: "RedBull",
+    url: "https://n1.sdlcdn.com/imgs/d/m/c/RED-BULL-Energy-Drink-Mixed-SDL959599751-1-769f8.jpg",
+  },
+  {
+    name: "Orange Juice",
+    url: "https://i5.walmartimages.com/asr/f5a3089f-4f7a-41d6-8fdb-9dc60f8682f9.8b6d67ef80057ea1b152825520fe152d.jpeg",
+  },
+  {
+    name: "Gatorade",
+    url: "https://cdn.gardengrocer.com/attachments/photos/high_res/1885.jpg?8013",
+  },
+  {
+    name: "Monster",
+    url: "https://images.jumpseller.com/store/la-carreta-supermarkets1/4795218/MonsterOriginal2.jpg?1596568155",
+  },
+
 ];
 
 module.exports = drinks;
