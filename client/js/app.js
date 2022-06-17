cb4d523eb71e3fb90f7f6c8fe055d47e0be91387
Ismael Ali <ismaelali78@outlook.com>
2022-06-17 16:15:57 +0100

    Final push

--- client/js/app.js
+++ client/js/app.js
@@ -2,24 +2,65 @@ let img = document.getElementById("drinkImg");
 
 let button = document.getElementById("btn");
 
+let nameInput = document.getElementById("nameInput");
+
+let urlInput = document.getElementById("urlInput");
+
+let submitBtn = document.getElementById("submitBtn");
+
+let checkURL = [];
+
 let counter = 0;
 
-const getDrink = async (counter) => {
+const getFirstDrink = async () => {
+  const response = await fetch(`http://localhost:3005/drinks`);
+  const data = await response.json();
+  img.setAttribute("src", data[0].url);
+};
+
+const getDrink = async () => {
   const response = await fetch(`http://localhost:3005/drinks`);
   const data = await response.json();
 
-  let allDrinks = data[counter].url;
-  img.setAttribute("src", allDrinks);
-
-  if (counter === data.length) {
+  if (counter == data.length) {
     counter = 0;
   }
+
+  let allDrinks = data[counter].url;
+  img.setAttribute("src", allDrinks);
 };
 
 function btnClick() {
   getDrink(counter);
   counter++;
-  console.log(counter);
 }
 
+getFirstDrink();
 button.addEventListener("click", btnClick);
+
+const addDrink = async (e) => {
+  e.preventDefault();
+
+  const drinkData = {
+    name: nameInput.value,
+    url: urlInput.value,
+  };
+
+  checkURL.push(JSON.stringify(drinkData));
+
+  console.log(checkURL);
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(drinkData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = await fetch("http://localhost:3005/drinks", options);
+  const data = await response.json();
+  console.log(data);
+};
+
+submitBtn.addEventListener("click", addDrink);
