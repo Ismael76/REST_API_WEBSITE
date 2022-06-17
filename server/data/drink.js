8121e1d54264153b24f8f1ec20ba6985568110b3
Ismael Ali <ismaelali78@outlook.com>
2022-06-17 14:23:32 +0100

    fixed client side, added few more pictures

--- server/data/drink.js
+++ server/data/drink.js
@@ -11,6 +11,14 @@ const drinks = [
     name: "Fanta Fruit Twist",
     url: "https://bwhdrinks.co.uk/wp-content/uploads/2016/11/Fanta-Fruit-Twist-500ml.jpg",
   },
+  {
+    name: "Dr Pepper",
+    url: "https://assets.sainsburys-groceries.co.uk/gol/3300527/1/640x640.jpg",
+  },
+  {
+    name: "Coke",
+    url: "https://delivery.pret.co.uk/dw/image/v2/ABBI_PRD/on/demandware.static/-/Sites-pret-uk-master-catalog/default/dw1342af06/products/Cold%20Drinks/UK001022_RegularBottle_01_607.jpg?sw=584&sh=584",
+  },
 ];
 
 module.exports = drinks;
